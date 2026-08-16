4393ee3f26ca7d8535b9758c2d3b13245fab90eb
Khoi <minkhoi.dev@gmail.com>
2026-08-16 22:56:02 +0700

    fix ci/cd test

--- src/backend/src/test/java/com/personal/finance/backend/BackendApplicationTests.java
+++ src/backend/src/test/java/com/personal/finance/backend/BackendApplicationTests.java
@@ -1,15 +1,17 @@
 package com.personal.finance.backend;
 
+import com.personal.finance.backend.config.TestMailConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestMailConfig.class)
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
